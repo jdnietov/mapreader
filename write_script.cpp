5142c457fc272f3fa3bf5bd4b1d1892a871a4c17
Jose Nieto <jdnv.123@gmail.com>
2017-06-25 00:54:12 -0500

    Take screenshots dinamically over map

--- write_script.cpp
+++ write_script.cpp
@@ -2,28 +2,46 @@
 #include <fstream>
 #include <stdlib.h>     /* system, NULL, EXIT_FAILURE */
 #include <string>
+#define LATTILES 4
+#define LONTILES 4
 
 using namespace std;
 
+const double DELTALAT = 0.016892;
+const double DELTALON = -0.032916;
+
 int main () {
   const string mapZoom = "16";
-  double lat = 4.7512149;
-  double lon = -74.0711512;
+  double ini_lat = 4.8166031;
+  double ini_lon = -74.0345367;
   ofstream bash;
 
   bash.open ("script.sh");
   bash.precision(9);
   bash << "#!/bin/bash\n";
-  bash << "google-chrome \'https://www.google.com.co/maps/@"
-    << lat << ","
-    << lon << ","
-    << mapZoom << "z/data=!5m1!1e1?hl=es-419\' &\n";
-  bash << "pid=$!\n";
-  bash << "sleep 4\n";
-  bash << "wmctrl -a chrome\n";
-  bash << "xdotool key Escape\nsleep 0.5\n";
-  bash << "import -window \"$(xdotool getwindowfocus -f)\" $(date +%F_%H%M%S_%N).png\n";
-  bash << "xdotool key Ctrl+w";
+
+  cout.precision(9);
+  double lat = ini_lat;
+  double lon = ini_lon;
+  for(int i = 0; i < LONTILES; i++) {
+    for(int j = 0; j < LATTILES; j++) {
+      cout << lat << ", " << lon << endl;
+
+      bash << "google-chrome \'https://www.google.com.co/maps/@"
+        << lat << ","
+        << lon << ","
+        << mapZoom << "z/data=!5m1!1e1?hl=es-419\' &\n";
+      bash << "pid=$!\n";
+      bash << "sleep 4\n";
+      bash << "wmctrl -a chrome\n";
+      bash << "xdotool key Escape\nsleep 0.5\n";
+      bash << "import -window \"$(xdotool getwindowfocus -f)\" $(date +%F_%H%M%S)_" << lat << "_" << lon << ".png\n";
+      bash << "xdotool key Ctrl+w\n";
+
+      lat -= DELTALAT;
+    }
+    lon += DELTALON;
+  }
   bash.close();
 
   system("chmod +x script.sh");
