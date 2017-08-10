34d54ed3fa7a8a42b878354eac37f86ccc53c5e5
José Nieto <jdnv.123@gmail.com>
2017-08-10 14:24:14 -0500

    Write to data.log, handle interruptions

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <ctime>
+#include <csignal>
 #include <fstream>
-#include <stdlib.h>
 #include <string>
+#include <stdlib.h>
 #include <errno.h>
+
 #include "tmatch.h"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+
 #define Q 77
 #define WINW 1920
 #define WINH 1080
@@ -19,19 +23,76 @@ const double DELTALNG = 0.0328303;  // x-axis, decrease left
 const char* IMGNAME = "screenshot.png";
 
 struct Location {
-  int x;
-  int y;
   double lat;
   double lng;
 };
 
+ofstream bash, data;
 struct Location grid[Q];
+
+void printPoint(float x, float y);
+void getCoordinates(int pixelLng, int pixelLat, double lat, double lng, void *res);
+void chksyscall(char* line);
+void fillCol(int init, double iniLat, double iniLng, int n);
+void initGrid();
+string currentDateTime();
+void signalHandler( int signum );
+void clean();
+
+int main () {
+  signal(SIGINT, signalHandler);
+
+  bash.precision(9);
+  data.precision(9);
+  cout.precision(9);
+
+  data.open("data.log", ios::app);
+  data << "*** " << currentDateTime() << " ***" << endl << endl;
+  data.close();
+
+  initGrid();
+
+  for(int i = 0; i < Q; i++) {
+    double lat = grid[i].lat, lng = grid[i].lng;
+
+    bash.open ("script.sh");
+    bash << "#!/bin/bash\n";
+    bash << "google-chrome --headless --disable-gpu --screenshot --window-size="
+        << WINW << "," << WINH
+        << " \'https://www.google.com.co/maps/@" << lat << "," << lng << ","
+        << ZOOM << "z/data=!5m1!1e1?hl=es-419\' &> /dev/null\n";
+    bash.close();
+
+    chksyscall( (char*)"chmod +x script.sh" );
+    chksyscall( (char*)"./script.sh" );
+
+    Point * p_point;
+    p_point = (Point*) readAndMatch( (char*) IMGNAME );
+
+    struct Location loc;
+    getCoordinates(p_point->x, p_point->y, lat, lng, &loc);
+
+    data.open("data.log", ios::app);
+    data << "(" << lat << ", " << lng << ")" << endl;
+    data << "-- [" << loc.lat << ", " << loc.lng << "]" << endl;
+    data.close();
+
+    bash.open ("script.sh");
+    bash << "#!/bin/bash\n";
+    bash << "rm " << IMGNAME << "\n";
+    bash.close();
+
+    chksyscall( (char*)"./script.sh" );
+  }
+
+  return 0;
+}
 
 void printPoint(float x, float y) {
   cout << "[" << x << "," << y << "]";
 }
 
-void getCoordinates(int pixelLng, int pixelLat, double lat, double lng) {
+void getCoordinates(int pixelLng, int pixelLat, double lat, double lng, void *res) {
   // image's origin coordinates
   double oLat = lat + DELTALAT/2;
   double oLng = lng - DELTALNG/2;
@@ -39,27 +100,31 @@ void getCoordinates(int pixelLng, int pixelLat, double lat, double lng) {
   double resLat = oLat - DELTALAT*((double) pixelLat/WINH);
   double resLng = oLng + DELTALNG*((double) pixelLng/WINW);
 
-  // cout << (double) pixelLat/WINH << ", " << pixelLng/WINW << endl;
   cout << "P: " << pixelLng << ", " << pixelLat << endl;
   cout << "C: "; printPoint(resLat, resLng); cout << endl << endl;
+
+  struct Location * locRes = (struct Location *) res;
+  locRes->lat = resLat;
+  locRes->lng = resLng;
 }
 
-
-int chkSysCall(char* line) {
-  int ret_val = system(line);
-  if (ret_val == 0 && errno == 0)
-  {
-    return 1;
+void chksyscall(char* line) {
+  int status = system(line);
+  if (status < 0) {
+    std::cout << "Error: " << strerror(errno) << '\n';
+    clean();
+    exit(-1);
   }
-  cout << line << ": error running " << line << endl;
-  return 0;
-}
-
-void writeBash(ofstream &file, double lat, double lng) {
-  file << "google-chrome --headless --disable-gpu --screenshot --window-size="
-      << WINW << "," << WINH
-      << " \'https://www.google.com.co/maps/@" << lat << "," << lng << ","
-      << ZOOM << "z/data=!5m1!1e1?hl=es-419\' &> /dev/null\n";
+  else
+  {
+    if (WIFEXITED(status)) {
+      std::cout << "Program returned normally, exit code " << WEXITSTATUS(status) << '\n';
+    } else {
+      clean();
+      std::cout << "Program exited abnormally\n";
+      exit(-1);
+    }
+  }
 }
 
 void fillCol(int init, double iniLat, double iniLng, int n) {
@@ -83,39 +148,26 @@ void initGrid() {
   fillCol(73, 4.6341032, -74.2228613, 4);
 }
 
-int main () {
-  ofstream bash;
+string currentDateTime() {
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+    // for more information about date/time format
+    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
 
-  bash.precision(9);
-  cout.precision(9);
+    return buf;
+}
 
-  initGrid();
+void signalHandler( int signum ) {
+   cout << "Interrupt signal (" << signum << ") received.\n";
+   clean();
+   exit(signum);
 
-  for(int i = 0; i < Q; i++) {
-    double lat = grid[i].lat, lng = grid[i].lng;
-    cout << "---";
-    printPoint(lat, lng);
-    cout << "---" << endl;
+}
 
-    bash.open ("script.sh");
-    bash << "#!/bin/bash\n";
-    writeBash(bash, lat, lng);
-    bash.close();
-
-    chkSysCall( (char*)"chmod +x script.sh" );
-    chkSysCall( (char*)"./script.sh" );
-
-    Point * p_point;
-    p_point = (Point*) readAndMatch( (char*) IMGNAME );
-    getCoordinates(p_point->x, p_point->y, lat, lng);
-
-    bash.open ("script.sh");
-    bash << "#!/bin/bash\n";
-    bash << "rm " << IMGNAME << "\n";
-    bash.close();
-
-    chkSysCall( (char*)"./script.sh" );
-  }
-
-  return 0;
+void clean() {
+  bash.close();
+  data.close();
 }
